7115831fa2b0ca4957d69848df5596ebf73a8a4c
hyeri <cos850@naver.com>
2024-06-14 23:38:53 +0900

    [백준] 9012 - 괄호

---
+++ src/baekjoon/datastructure/9012.kt
@@ -0,0 +1,38 @@
+package baekjoon.datastructure
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+/**
+ * 9012번 "괄호"
+ */
+val yes = "YES"
+val no = "NO"
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
+    val n = readLine().toInt()
+
+    // execute
+    for(i in 1 .. n)
+        println(validateBracket(readLine()))
+}
+
+fun validateBracket(str: String): String{
+    if(str.isBlank()) return no
+
+    val stack : Stack<Char> = Stack()
+
+    try {
+        for(char in str.toCharArray()){
+            when(char){
+                '(' -> stack.push(char)
+                ')' -> stack.pop()
+                else -> return no
+            }
+        }
+        return if(stack.isEmpty()) yes else no
+    } catch (e: EmptyStackException) {
+        return no
+    }
+}
