36a17cb675d4bda38591754b5213f54eeb64cd9a
Hyeri Jeong <cos850@naver.com>
2024-06-20 23:14:03 +0900

    [백준] 1158 - 요세푸스 (큐)

---
+++ src/baekjoon/datastructure/1158.kt
@@ -0,0 +1,30 @@
+package baekjoon.datastructure
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.StringBuilder
+import java.util.LinkedList
+import java.util.Queue
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val cmd = readLine().split(' ')
+    val n = cmd[0].toInt()
+    val k = cmd[1].toInt()
+    val queue : Queue<Int> = LinkedList()
+
+    for(i in 1 .. n){
+        queue.offer(i)
+    }
+
+    val sb = StringBuilder().append("<")
+
+    while(queue.isNotEmpty()) {
+        repeat(k-1) {
+            val temp = queue.poll()
+            queue.add(temp)
+        }
+        sb.append(queue.poll()).append(", ")
+    }
+
+    println(sb.deleteRange(sb.length-2, sb.length).append(">").toString())
+}
