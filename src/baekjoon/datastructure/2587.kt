c27a8bad51b11d8505a4f6d4e2f1f318cae51815
Hyeri Jeong <cos850@naver.com>
2024-06-25 00:30:02 +0900

    [백준] 2587 - 대표값2

---
+++ src/baekjoon/datastructure/2587.kt
@@ -0,0 +1,17 @@
+package baekjoon.datastructure
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
+    var sum = 0
+    val list = mutableListOf<Int>()
+
+    repeat(5) {
+        val n = readLine().toInt()
+        list.add(n)
+        sum += n
+    }
+    list.sort()
+    println("${sum/5}\n${list[2]}")
+}
